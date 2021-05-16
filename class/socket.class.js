6ccf8605cf955af43e55c8b69b9dba29d63fbfcf
Daniel Pullan <dan@3264.uk>
2021-05-16 17:43:21 +0100

    Socket data, minor styling fix
    Removed textarea styling, made buttons nicer.

--- class/socket.class.js
+++ class/socket.class.js
@@ -2,7 +2,7 @@ const io = require('socket.io-client');
 
 let thisclass = {
   Init: function() {
-    conn = io(`ws://localhost:3000`, {forceNew:true});
+    conn = io(`ws://talk.3264.uk`, {forceNew:true});
 
     conn.on("connect", () => {
       console.log("Connection Established");
